3892698c4c81abcf262d8e80dd00810db8e19a71
Gabriela Aguilar <contacto.gabriela.aguilar@gmail.com>
2024-09-30 23:19:34 -0500

    cv actualizado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -102,7 +102,7 @@ export const Navbar = () => {
             
             <li>
               <Link
-                href="/GabrielaAguilar2024.pdf"
+                href="/gabrielaaguilar-english.pdf"
                 className={`block  ${!isSticky ? "sm:text-white" : "text-white" }  bg-purple hover:bg-black text-white py-2 px-6 rounded-[35px] md:border-0 transition-all duration-300 `} target="_blank"
               >
                 Download CV
